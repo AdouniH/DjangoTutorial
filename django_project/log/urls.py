520ca6bf40517c9fffbf858b7ef27aff1dfd681f
adouniH <Houssemadouni11@gmail.com>
2019-08-21 21:11:32 +0200

    save changes

--- django_project/log/urls.py
+++ django_project/log/urls.py
@@ -1,10 +1,11 @@
 
 
 from django.urls import path
-from .views import main_page, disconnect
+from .views import main_page, disconnect, signup
 
 
 urlpatterns = [
     path('', main_page, name='main_page'),
+    path('signup/', signup, name='signup'),
     path('disconnect/', disconnect, name='disconnect')
 ]
--- django_project/log/views.py
+++ django_project/log/views.py
@@ -3,31 +3,40 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate
 from django.contrib.auth import logout, login
 from .form import Login_form
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponse
 
 def main_page(request):
-    form = Login_form()
 
-    # login
-    if request.POST:
-        user = authenticate(username=request.POST["user_name"], password=request.POST["user_pwd"])
-        if user:
-            login(request, user)
+    if not request.user.is_authenticated:
 
-    # get IP
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-    if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[0]
+        form = Login_form()
+
+        # login
+        if request.method == 'POST':
+            usor = authenticate(username=request.POST["user_name"], password=request.POST["user_pwd"])
+            if usor:
+                login(request, usor)
+                return render(request, 'home.html', {})
+
+        return render(request, 'homepage.html', {"form": form})
+
+    return render(request, 'home.html', {})
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            usor = authenticate(username=request.POST["user_name"], password=request.POST["user_pwd"])
+            login(request, usor)
+            return render(request, 'home.html', {'form': form})
+        else:
+            return HttpResponse("NO")
     else:
-        ip = request.META.get('REMOTE_ADDR')
-
-    # return variables to template
-    context = {"form": form,
-               "user": request.user.username,
-               "auth": request.user.is_authenticated,
-               "ip": ip
-               }
-
-    return render(request, 'homepage.html', context)
+        form = UserCreationForm()
+    return render(request, 'sign_up.html', {'form': form})
 
 def disconnect(request):
     logout(request)
